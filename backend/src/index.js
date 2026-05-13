bdafdda87e658a6b0ec25ca571e6c7be1b8ca2e0
Shashi raj <shashirajt20@gmail.com>
2026-05-13 23:53:23 +0530

    feat : installed websocket

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ import cors from "cors"
 import dotenv from "dotenv"
 import router from "./routes/auth.route.js";
 import shipmentrouter from "./routes/shipment.route.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 
 dotenv.config();
 const app = express();
@@ -12,11 +15,21 @@ app.use(cors({
 }))
 app.use(express.json());
 
+const server = createServer(app);
 
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+});
 
 app.use("/api/", router);
 app.use("/shipment/", shipmentrouter);
 
-app.listen(process.env.PORT, ()=>{
+server.listen(process.env.PORT, ()=>{
     console.log(`Server is running on ${process.env.PORT}`);
 })
